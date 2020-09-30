2590a9e5fa5204844f261b1d7e41c15adb1f7410
14808 <14808@LAPTOP-6LI19729>
2020-09-30 14:08:20 -0700

    corrected AddingMachine and added a Main.java to test the AddingMachine.java class

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -4,23 +4,29 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  private String history;
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    history = "0";
   }
   
   public int getTotal () {
-    return 0;
+    return total;
   }
   
   public void add (int value) {
+	  total += value;
+	  history += " + " + "value";
   }
 
   public void subtract (int value) {
+	  total -= value;
+	  history += " - " + "value";
   }
 
   public String toString () {
-    return "";
+    return history;
   }
 
   public void clear() {
---
+++ cse360assignment02/src/cse360assignment02/Main.java
@@ -0,0 +1,10 @@
+package cse360assignment02;
+
+public class Main{
+	public static void main(String[] args) {
+		AddingMachine calculator = new AddingMachine();
+		calculator.add(1);
+		calculator.add(2);
+		calculator.subtract(5);
+	}
+}
